9379b510df3aef237d3c92e3afdcfc7eebc6cb83
shina <shina.blueprint@gmail.com>
2026-01-11 15:18:00 +0900

    Add PowerShell profile

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+if (Get-Command mise -ErrorAction SilentlyContinue) {
+    (& mise activate pwsh) | Out-String | Invoke-Expression
+}
+
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    (& zoxide init powershell) | Out-String | Invoke-Expression
+}
